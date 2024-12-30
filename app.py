a17715f834bb18e30dd3d182aae5219037b8896b
demarijm <demarijmiller@gmail.com>
2024-12-29 21:50:12 -0500

    New comp added from shortcut

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ import pandas as pd
 
 app = Flask(__name__)
 
+
+@app.route('/health', methods=['GET'])
+def health():
+    return jsonify({"status": "healthy"})
+
 @app.route('/anomaly', methods=['POST'])
 def anomaly_detection():
     """
